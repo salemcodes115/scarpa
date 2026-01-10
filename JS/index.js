10c1713570a0de4b87319bcba94fc1ba5abd0620
salemcodes115 <anjolaoluwa115@gmail.com>
2026-01-10 12:39:00 +0100

    inital commit

---
+++ JS/index.js
@@ -0,0 +1,57 @@
+//    function addtocart() {
+//        alert('you have added item to cart')
+//  }
+
+// alert('hello world')
+// prompt('Enter your Score')
+// confirm('do you want  to exit')
+// console.log('welcome to Javascript')
+
+// variables
+// var
+// let
+// const
+
+// let name = 'Jose'
+// name = 'David'
+// console.log(name)
+
+// const age = 18
+// age = 20
+// console.log(age)
+
+// data type
+// primitive dsts type
+
+// Number data type
+// let milege = 20031
+// console.log(milage)
+
+// String data type
+// let address= "230 jones street"
+// console.log(address)
+
+// Boolean data type
+// let isCertified = false
+// console.log(username)
+
+// Null data type
+// let username= null
+// console.log(username)
+
+// undefined data type
+// let time;
+// console.log(time)
+
+
+
+
+// Non-primitive data type
+// data  data type
+let data = new data ()
+console.log(data)
+
+// function data type 
+function addNum (a, b){
+    console.log(a + b)
+}
